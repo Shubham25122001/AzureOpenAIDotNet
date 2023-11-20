a972c785c8e6e9f0d3f08fda87de3368c87e4454
shubham verma <101853403+Shubham25122001@users.noreply.github.com>
2023-11-20 15:59:10 +0530

    Pdf ReadComplete

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,46 +1,91 @@
-using System.Diagnostics;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using DotNetConf.Models;
+using Microsoft.Extensions.Logging;
 using Azure.AI.OpenAI;
+using Azure.Core;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
 using Azure;
 
-namespace DotNetConf.Controllers;
-
-public class ChatController : Controller
+namespace DotNetConf.Controllers
 {
-    string endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
-    string key = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
-    string model = Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL");
-
-    private readonly ILogger<HomeController> _logger;
-
-    public ChatController(ILogger<HomeController> logger)
+    public class ChatController : Controller
     {
-        _logger = logger;
+        private readonly ILogger<ChatController> _logger;
+        private readonly string _endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+        private readonly string _key = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
+        private readonly string _model = Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL");
+
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetResponse(string userMessage)
+        {
+            OpenAIClient client = new OpenAIClient(new Uri(_endpoint), new AzureKeyCredential(_key));
+            var chatCompletionOptions = new ChatCompletionsOptions()
+            {
+                Messages = {
+                    new ChatMessage(ChatRole.System, "You are a helpful AI assistant"),
+                    new ChatMessage(ChatRole.User, "Does Azure support GPT-4 ?"),
+                    new ChatMessage(ChatRole.Assistant, "Yes, it does"),
+                    new ChatMessage(ChatRole.User, userMessage)
+                },
+                MaxTokens = 400
+            };
+
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(deploymentOrModelName: _model, chatCompletionOptions);
+            var botResponse = response.Value.Choices.First().Message.Content;
+            return Json(new { response = botResponse });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetResponseFromPdf(string userMessage)
+        {
+            OpenAIClient client = new OpenAIClient(new Uri(_endpoint), new AzureKeyCredential(_key));
+            string pdfText = GetText("Data/azure1.pdf");
+            var chatCompletionOptions = new ChatCompletionsOptions()
+            {
+                Messages = {
+                    new ChatMessage(ChatRole.System, "You are a helpful AI assistant"),
+                    new ChatMessage(ChatRole.User, "The following information is from the PDF text: " + pdfText),
+                    new ChatMessage(ChatRole.User, userMessage)
+                },
+                MaxTokens = 400,
+                Temperature = 0
+            };
+
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(deploymentOrModelName: _model, chatCompletionOptions);
+            var botResponse = response.Value.Choices.First().Message.Content;
+            return Json(new { response = botResponse });
+        }
+
+        private static string GetText(string pdfFilePath)
+        {
+            using (PdfDocument pdfDoc = new PdfDocument(new PdfReader(pdfFilePath)))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
+                {
+                    PdfPage pdfPage = pdfDoc.GetPage(page);
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string currentText = PdfTextExtractor.GetTextFromPage(pdfPage, strategy);
+                    text.Append(currentText);
+                }
+
+                return text.ToString();
+            }
+        }
     }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> GetResponse(string userMessage)
-    {
-        OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
-        var chatCompletionOptions = new ChatCompletionsOptions(){
-            Messages ={
-                new ChatMessage(ChatRole.System, "You are a helpful AI assistant"),
-                new ChatMessage(ChatRole.User, "Does Azure support GPT-4 ?"),
-                new ChatMessage(ChatRole.Assistant, "Yes it does"),
-                new ChatMessage(ChatRole.User, userMessage)
-            },
-            MaxTokens = 400
-        };
-
-        Response<ChatCompletions> response = await client.GetChatCompletionsAsync(deploymentOrModelName: model, chatCompletionOptions);
-        var botRespones = response.Value.Choices.First().Message.Content;
-        return Json(new { response = botRespones });   
-    }
-
 }
